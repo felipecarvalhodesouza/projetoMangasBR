e5e30eb4bc0a58ecb1185a4c644c4b1dbbe758f0
Felipe Carvalho <desouzafelipecarvalho@gmail.com>
2019-02-18 22:35:08 -0300

    Criando operação de instanciação Publisher

--- src/main/java/com/felipecarvalho/projetoMangasBR/ProjetoMangasBrApplication.java
+++ src/main/java/com/felipecarvalho/projetoMangasBR/ProjetoMangasBrApplication.java
@@ -1,13 +1,34 @@
 package com.felipecarvalho.projetoMangasBR;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.felipecarvalho.projetoMangasBR.domain.Publisher;
+import com.felipecarvalho.projetoMangasBR.repositories.PublisherRepository;
+
 @SpringBootApplication
-public class ProjetoMangasBrApplication {
+public class ProjetoMangasBrApplication implements CommandLineRunner{
+	
+	@Autowired
+	private PublisherRepository publisherRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoMangasBrApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Publisher p1 = new Publisher(null, "Panini");
+		Publisher p2 = new Publisher(null, "JBC");
+		Publisher p3 = new Publisher(null, "New Pop");
+		
+		publisherRepository.saveAll(Arrays.asList(p1,p2,p3));
+		
 	}
 
 }
