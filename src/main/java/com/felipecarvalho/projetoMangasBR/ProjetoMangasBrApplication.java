c2f6e492dea06d33dd074200f8ff48f77cc71812
Felipe Carvalho <desouzafelipecarvalho@gmail.com>
2019-02-18 21:47:45 -0300

    Projeto criado

---
+++ src/main/java/com/felipecarvalho/projetoMangasBR/ProjetoMangasBrApplication.java
@@ -0,0 +1,13 @@
+package com.felipecarvalho.projetoMangasBR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoMangasBrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoMangasBrApplication.class, args);
+	}
+
+}
