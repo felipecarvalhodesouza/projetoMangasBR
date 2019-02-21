8a23ea4728d5127cdf4d88b2db08cfc7771f6f02
Felipe Carvalho <desouzafelipecarvalho@gmail.com>
2019-02-20 22:17:59 -0300

    Correção de imports de Title Repository

--- src/main/java/com/felipecarvalho/projetoMangasBR/repositories/TitleRepository.java
+++ src/main/java/com/felipecarvalho/projetoMangasBR/repositories/TitleRepository.java
@@ -1,15 +1,9 @@
 package com.felipecarvalho.projetoMangasBR.repositories;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.felipecarvalho.projetoMangasBR.domain.Title;
-import com.felipecarvalho.projetoMangasBR.domain.Volume;
 
 @Repository
 public interface TitleRepository extends JpaRepository<Title, Integer>{
