2ddabba22f5e92d47d513fdccef877ebb6f45fc0
Felipe Carvalho <desouzafelipecarvalho@gmail.com>
2019-03-03 10:06:16 -0300

    Paginação com parâmetros opcionais na requisição

--- src/main/java/com/felipecarvalho/projetoMangasBR/resources/PublisherResource.java
+++ src/main/java/com/felipecarvalho/projetoMangasBR/resources/PublisherResource.java
@@ -4,16 +4,17 @@ import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.felipecarvalho.projetoMangasBR.domain.Publisher;
-import com.felipecarvalho.projetoMangasBR.domain.Title;
 import com.felipecarvalho.projetoMangasBR.services.PublisherService;
 
 @RestController
@@ -41,6 +42,16 @@ public class PublisherResource {
 	@RequestMapping(method=RequestMethod.GET)
 	public ResponseEntity<List<Publisher>> findAll() {
 		List<Publisher> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@RequestMapping(value="/page", method=RequestMethod.GET)
+	public ResponseEntity<Page<Publisher>> findPage(
+			@RequestParam(value="page", defaultValue="0")Integer page,
+			@RequestParam(value="linesPerPage", defaultValue="24") Integer linesPerPage,
+			@RequestParam(value="orderBy", defaultValue="name")String orderBy,
+			@RequestParam(value="direction", defaultValue="ASC")String direction) {
+		Page<Publisher> list = service.findPage(page, linesPerPage, orderBy, direction);
 		return ResponseEntity.ok().body(list);
 	}
 	
--- src/main/java/com/felipecarvalho/projetoMangasBR/services/PublisherService.java
+++ src/main/java/com/felipecarvalho/projetoMangasBR/services/PublisherService.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.felipecarvalho.projetoMangasBR.domain.Publisher;
@@ -24,6 +27,11 @@ public class PublisherService {
 	
 	public List<Publisher> findAll(){
 		return repo.findAll();
+	}
+	
+	public Page<Publisher> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);			
 	}
 	
 	public Publisher insert(Publisher obj) {
