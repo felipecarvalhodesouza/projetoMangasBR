ae0dc816a11c626177ae981f03248c2a162e2ec3
Felipe Carvalho <desouzafelipecarvalho@gmail.com>
2019-04-29 21:37:19 -0300

    Correção Security Config

--- src/main/java/com/felipecarvalho/projetoMangasBR/config/SecurityConfig.java
+++ src/main/java/com/felipecarvalho/projetoMangasBR/config/SecurityConfig.java
@@ -52,7 +52,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	
 	private static final String[] PUBLIC_MATCHERS_POST = {
 			"/users",
-			"/auth/forgot/**"
+			"/auth/**"
 	};
 
 	@Override
